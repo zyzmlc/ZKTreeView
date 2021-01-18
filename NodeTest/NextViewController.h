4d67a52081b18cfeeb3cf6836b64ef13dc00d49e
小冬 <xiaodong@xiaodongdeMacBook-Pro.local>
2021-01-18 15:10:02 +0800

    first commit

--- NodeTest/NextViewController.h
+++ NodeTest/NextViewController.h
@@ -1,10 +1,4 @@
-//
-//  NextViewController.h
-//  NodeTest
-//
-//  Created by 小冬 on 2021/1/17.
-//  Copyright © 2021 小冬. All rights reserved.
-//
+
 
 #import <UIKit/UIKit.h>
 
--- NodeTest/NextViewController.m
+++ NodeTest/NextViewController.m
@@ -1,178 +1,107 @@
-//
-//  NextViewController.m
-//  NodeTest
-//
-//  Created by 小冬 on 2021/1/17.
-//  Copyright © 2021 小冬. All rights reserved.
-//
+
 
 #import "NextViewController.h"
-#import "TreeView.h"
-#import "ViewController.h"
+#import "NSString+Hash.h"
+#import "EncryptAESTool.h"
+#import "EncryptRsaTool.h"
 
-// 判断是否为iPhone x 或者 xs
-#define iPhoneX [[UIScreen mainScreen] bounds].size.width == 375.0f && [[UIScreen mainScreen] bounds].size.height == 812.0f
-// 判断是否为iPhone xr 或者 xs max
-#define iPhoneXR [[UIScreen mainScreen] bounds].size.width == 414.0f && [[UIScreen mainScreen] bounds].size.height == 896.0f
-// 是全面屏手机
-#define isFullScreen (iPhoneX || iPhoneXR)
-
-// 全面屏适配 适配
-// 状态栏高度
-#define kStateBarHeight (isFullScreen ? 44.0 : 20.0)
-// 导航栏高度
-#define kNavigationBarHeight (kStateBarHeight + 44.0)
-// 底部tabbar高度
-#define kTabBarHeight (isFullScreen ? (49.0+34.0) : 49.0)
-
-#define TabbarSafeBottomMargin     (iPhoneX ? 34.f : 0.f)
-
-
-#define cellHeight 60
-
-
-@interface NextViewController () <TreeViewDelegate>
-
-@property (nonatomic,strong)  TreeView     *treeView;
-@property (nonatomic,strong)  TreeNode     *rootNode;
-
+@interface NextViewController ()
 @end
 
 @implementation NextViewController
 
 - (void)viewDidLoad {
-    [super viewDidLoad];
-    self.view.backgroundColor = [UIColor whiteColor];
-    self.title = @"集团";
-    
-    TreeView *treeView = [[TreeView alloc] init];
-    treeView.backgroundColor = [UIColor whiteColor];
-    treeView.action = self;
-    treeView.showsVerticalScrollIndicator = NO;
-    treeView.frame = CGRectMake(0, kStateBarHeight, [UIScreen mainScreen].bounds.size.width, [UIScreen mainScreen].bounds.size.height -  kStateBarHeight );
-    treeView.separatorStyle = UITableViewCellSeparatorStyleNone;
-    [self.view addSubview:treeView];
-    _treeView = treeView;
     
     
-    [self setupData];
-    // 设置数据源
-    _treeView.treeSource = self.rootNode;
-    [_treeView reloadData];
 }
 
-#pragma mark - UITreeViewDelegate
-- (CGFloat)treeView:(TreeView *)treeView heightForRowAtIndexPath:(NSIndexPath *)indexPath
+- (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event{
+    
+        [self testHash];
+    
+        [self testAES];
+    
+    //1.加载公钥
+//    [[EncryptRsaTool sharedRSACryptor] generateKeyPair:512];
+    [[EncryptRsaTool sharedRSACryptor] loadPublicKey:[[NSBundle mainBundle] pathForResource:@"rsacert.der" ofType:nil]];
+//    //2.加载私钥
+    [[EncryptRsaTool sharedRSACryptor] loadPrivateKey: [[NSBundle mainBundle] pathForResource:@"p-123.p12" ofType:nil] password:@"123"];
+    
+    [self testRSA];
+}
+
+- (void)testHash{
+    //密码
+    NSString * pwd = @"123456";
+    
+    /*
+     1: MD5 直接加密 e10adc3949ba59abbe56e057f20f883e
+     不足：不够安全了。可以反查询！
+     验证： md5 -s "123456"
+     */
+    NSString *encryString = pwd.md5String;
+    NSLog(@"MD5: %@",encryString);
+    
+    /*
+     2：MD5 加盐（动态盐）  b4f7127cca2309b8f1bf06308c2b0c3d
+     弊端： 盐是固定的，写死在程序里面，一旦泄露就不安全了！
+     验证： md5 -s "123456second"
+     */
+    NSString *encryString1 = [pwd stringByAppendingString:@"second"].md5String;
+    NSLog(@"MD5加盐: %@",encryString1);
+    
+    
+    /* 3：HMAC  ed958e95cc411a0f71b4234390438ae3
+     使用一个密钥加密，并且做两次散列！
+     在实际开发中，密钥（KEY）来自于服务器（动态的）！
+     一个账号，对应一个KEY，而且还可以跟新！
+     验证：echo -n "123456" | openssl dgst -md5 -hmac "hello"
+     */
+    NSString *encryString2 = [pwd hmacMD5StringWithKey:@"hello"];
+    NSLog(@"HMAC加秘钥：%@",encryString2);
+}
+
+- (void)testAES{
+    /** AES - ECB */
+    NSString * key = @"abc";
+    NSString *message = @"hello";
+    /*  1：AES 加密 ecb
+     d1QG4T2tivoi0Kiu3NEmZQ==
+     echo -n hello | openssl enc -aes-128-ecb -K 616263 -nosalt | base64
+     */
+    NSString * encStr = [[EncryptAESTool sharedEncryptionTools] encryptString:message keyString:key iv:nil];
+    NSLog(@"ECB加密：%@",encStr);
+    
+    /* 2：AES 解密 ecb
+     echo -n d1QG4T2tivoi0Kiu3NEmZQ== | base64 -D | openssl enc -aes-128-ecb -K 616263 -nosalt -d
+     */
+    NSLog(@"ECB解密：%@",[[EncryptAESTool sharedEncryptionTools] decryptString:encStr keyString:key iv:nil]);
+    
+    
+    /* 3：AES - CBC 加密  u3W/N816uzFpcg6pZ+kbdg==
+     echo -n hello | openssl enc -aes-128-cbc -iv 0102030405060708 -K 616263 -nosalt | base64
+     616263就是abc
+     */
+    uint8_t iv[8] = {1,2,3,4,5,6,7,8};
+    NSData * ivData = [NSData dataWithBytes:iv length:sizeof(iv)];
+    NSLog(@"CBC加密：%@",[[EncryptAESTool sharedEncryptionTools] encryptString:message keyString:key iv:ivData]);
+    
+    /* 4：解密 AES CBC 密码分组链接 iv向量
+     echo -n u3W/N816uzFpcg6pZ+kbdg== | base64 -D | openssl enc -aes-128-cbc -iv 0102030405060708 -K 616263 -nosalt -d
+     */
+    NSLog(@"CBC解密：%@",[[EncryptAESTool sharedEncryptionTools] decryptString:@"u3W/N816uzFpcg6pZ+kbdg==" keyString:key iv:ivData]);
+}
+
+- (void)testRSA
 {
-    return cellHeight;
+    
+    NSData * result = [[EncryptRsaTool sharedRSACryptor] encryptData:[@"hello" dataUsingEncoding:NSUTF8StringEncoding]];
+    //base64编码
+    NSString * base64 = [result base64EncodedStringWithOptions:0];
+    NSLog(@"RSA公钥加密:%@\n",base64);
+    
+    // 解密
+    NSData * dcStr = [[EncryptRsaTool sharedRSACryptor] decryptData:result];
+    NSLog(@"私钥解密：%@",[[NSString alloc] initWithData:dcStr encoding:NSUTF8StringEncoding]);
 }
-- (BOOL)treeView:(TreeView *)treeView clickAtTreeNode:(TreeNode *)node /* yes表示此行会被选中 */
-{
-    if(node.type == NSTreeNodeTypeObject)
-    {
-        NSLog(@"名字：%@", node.data);
-        ViewController *vc = [ViewController new];
-        vc.title = node.data;
-        [self.navigationController pushViewController:vc animated:YES];
-    }
-    return YES;
-}
-
-- (UITableViewCell *)treeView:(TreeView *)treeView cellAtTreeNode:(TreeNode *)node atIndexPath:(NSIndexPath *)indexPath;
-{
-    static NSString *kCellIdentifier = @"kCellIdentifier";
-    UITableViewCell *cell = [treeView dequeueReusableCellWithIdentifier:kCellIdentifier];
-    if (!cell)
-        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:kCellIdentifier];
-    else for (UIView *view in cell.contentView.subviews)
-        [view removeFromSuperview];
-    
-    cell.backgroundColor = [UIColor whiteColor];
-    cell.selectionStyle =  UITableViewCellSelectionStyleNone;
-    
-    
-    float w = treeView.bounds.size.width;
-    float h = cellHeight;
-    
-    // 背景, 分割线
-    NSInteger level = [node level];
-    NSInteger offset = level * 30 ;
-    //  根结点的level是0
-    
-    UIImageView* separator = [[UIImageView alloc] initWithFrame:CGRectMake(offset, cellHeight - 1, w - offset - 10, 1)];
-    separator.backgroundColor = [UIColor lightGrayColor];
-    [cell.contentView addSubview:separator];
-    
-    
-    // 展开/收起
-    UIImageView* headIcon = [[UIImageView alloc] initWithFrame:CGRectMake(offset, (cellHeight - 20 )/2, 2*10, 2*10)];
-    if (node.type == NSTreeNodeTypeShrink)
-        headIcon.image = [UIImage imageNamed:@"arrow_down"];
-    if (node.type == NSTreeNodeTypeExpand)
-        headIcon.image = [UIImage imageNamed:@"arrow_up"];
-    if (node.type == NSTreeNodeTypeObject){
-        headIcon.image = [UIImage imageNamed:@"icon_head"];
-        headIcon.frame = CGRectMake(offset, 10, 40, 40);
-        
-    }
-    [cell.contentView addSubview:headIcon];
-    
-    // 标题
-    UILabel* textLabel = [[UILabel alloc] initWithFrame:CGRectMake(CGRectGetMaxX(headIcon.frame) + 10 , 0, w - CGRectGetMaxX(headIcon.frame) - 20 , h - 1)];
-    textLabel.textColor = [UIColor blackColor];
-    textLabel.backgroundColor = [UIColor whiteColor];
-    [cell.contentView addSubview:textLabel];
-    
-    
-    textLabel.text = node.data;
-    
-    return cell;
-}
-
-
-#pragma mark - 数据
-
-- (void)setupData{
-    
-    NSArray *companyArray = @[@"集团一", @"集团二", @"集团三"];
-    NSArray *departArray = @[@"部门一", @"部门二", @"部门三", @"部门四"];
-    NSArray *departChildArray = @[@"子部门一", @"子部门二"];
-    NSArray *personArray = @[@"张三", @"李四", @"王五"];
-    
-    // 根节点
-    TreeNode *rootNode = [TreeNode createRootNode];
-    
-    for(int i = 0; i < companyArray.count; i++){
-        // 总公司一
-        TreeNode *companyNode = [[TreeNode alloc] initWithData:companyArray[i] type:NSTreeNodeTypeShrink parent:rootNode];
-        [rootNode insertChildNode:companyNode];
-        
-        for(int m = 0; m < departArray.count; m++){
-            // 部门一
-            TreeNode *departNode = [[TreeNode alloc] initWithData:departArray[m] type:NSTreeNodeTypeShrink parent:companyNode];
-            [companyNode insertChildNode:departNode];
-            
-            if(m == 1){
-                for (int j = 0; j < departChildArray.count; j ++) {
-                    TreeNode *deparChildtNode = [[TreeNode alloc] initWithData:departChildArray[j] type:NSTreeNodeTypeShrink parent:departNode];
-                    [departNode insertChildNode:deparChildtNode];
-                    
-                    for (int n = 0; n < personArray.count; n++) {
-                         TreeNode *personNode = [[TreeNode alloc] initWithData:personArray[n] type:NSTreeNodeTypeObject parent:deparChildtNode];
-                         [deparChildtNode insertChildNode:personNode];
-                    }
-                }
-            }else{
-                for (int n = 0; n < personArray.count; n++) {
-                     TreeNode *personNode = [[TreeNode alloc] initWithData:personArray[n] type:NSTreeNodeTypeObject parent:departNode];
-                     [departNode insertChildNode:personNode];
-                }
-            }
-        }
-    }
-    
-    self.rootNode = rootNode;
-}
-
-
 @end
